1fc81e54c1f59275f498cd7bcef85f55aa21cf3e
EduardoVaca <eduardo.vaca.g@gmail.com>
2017-11-02 12:30:20 -0600

    Add functionality for stopwatch

--- ToDoNow/Controller/StopWatchViewController.swift
+++ ToDoNow/Controller/StopWatchViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class StopWatchViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var timer = Timer()
+    var seconds = 0
+    var isTimerRunning = false
+    var resumeTapped = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +28,43 @@ class StopWatchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func runStopWatch() {
+        isTimerRunning = true
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(StopWatchViewController.updateTimer)), userInfo: nil, repeats: true)
     }
-    */
-
+    
+    @objc func updateTimer() {
+        seconds += 1
+        timeLabel.text = timeString(time: TimeInterval(seconds))
+    }
+    
+    @IBAction func startPressed(_ sender: Any) {
+        if !isTimerRunning {
+            runStopWatch()
+        }
+    }
+    
+    @IBAction func resetPressed(_ sender: Any) {
+        timer.invalidate()
+        seconds = 0
+        timeLabel.text = timeString(time: TimeInterval(seconds))
+        isTimerRunning = false
+    }
+    
+    @IBAction func pausedPressed(_ sender: Any) {
+        if !resumeTapped {
+            timer.invalidate()
+            resumeTapped = true
+        } else {
+            runStopWatch()
+            resumeTapped = false
+        }
+    }
+    
+    func timeString(time: TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format: "%02i:%02i:%02i", arguments: [hours, minutes, seconds])
+    }
 }
