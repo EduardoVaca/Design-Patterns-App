e3dfa7745a4ed4f67ad017445b13d9f1635f6380
EduardoVaca <eduardo.vaca.g@gmail.com>
2017-11-13 12:22:27 -0600

    Add doc of TimerVC

--- ToDoNow/Controller/TimerViewController.swift
+++ ToDoNow/Controller/TimerViewController.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+/**
+ Controller responsible for StopWatch functionality
+ Listen to UI events and refresh UI based upon them
+ Two cases:
+     - Just timer
+     - Timer with a ToDo Item
+ */
 class TimerViewController: UIViewController {
 
     @IBOutlet weak var hoursTextField: UITextField!
@@ -21,17 +28,14 @@ class TimerViewController: UIViewController {
     var resumeTapped = false
     var item: Item?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         customView()
     }
     
+    /**
+     Customs UI based on the existance of a ToDo Item
+     */
     func customView() {
         if let item = item {
             hoursTextField.isEnabled = false
@@ -54,6 +58,10 @@ class TimerViewController: UIViewController {
         }
     }
     
+    /**
+     Method that set the total of seconds based on hours, minutes and seconds inserted
+     - Returns: true if fields are valid, false if not
+     */
     func setTotalSeconds() -> Bool {
         if let seconds = secondsTextField.text,
             let secondsValue = Int(seconds),
@@ -67,11 +75,17 @@ class TimerViewController: UIViewController {
         return false
     }
     
+    /**
+     Runs timer scheduling a func to run every second
+     */
     func runTimer() {
         isTimerRunning = true
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(TimerViewController.updateTimer)), userInfo: nil, repeats: true)
     }
     
+    /**
+     Function to be called by timer to update UI or present alert if time is up
+     */
     @objc func updateTimer() {
         if seconds < 0 {
             timer.invalidate()
@@ -85,13 +99,19 @@ class TimerViewController: UIViewController {
             seconds -= 1
         }        
     }
-
+    
+    /**
+     Method that listens to the pressing of the start button
+     */
     @IBAction func start(_ sender: Any) {
         if !isTimerRunning && setTotalSeconds(){
             runTimer()
         }
     }
     
+    /**
+     Method that listens to the pressing of the pause button
+     */
     @IBAction func pausePressed(_ sender: Any) {
         if !resumeTapped {
             timer.invalidate()
@@ -102,6 +122,9 @@ class TimerViewController: UIViewController {
         }
     }
     
+    /**
+     Method that listens to the pressing of the reset button
+     */
     @IBAction func resetPressed(_ sender: Any) {
         timer.invalidate()
         if setTotalSeconds() {
